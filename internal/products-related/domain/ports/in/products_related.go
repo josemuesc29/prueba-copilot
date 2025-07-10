fef9329844fc3fa4d570efc8d4d9b895fdcb1217
joseMulato <jose.mulato.ext@farmatodo.com>
2025-07-10 09:36:14 -0500

    a

--- internal/products-related/domain/ports/in/products_related.go
+++ internal/products-related/domain/ports/in/products_related.go
@@ -1,12 +1,12 @@
 package in
 
-//go:generate mockgen -source=same_brand.go -destination=../../../../../test/mocks/same-brand/domain/ports/in/same_brand_mock.go
+//go:generate mockgen -source=products_related.go -destination=../../../../../test/mocks/products-related/domain/ports/in/products_related_mock.go
 
 import (
-	"ftd-td-catalog-item-read-services/internal/same-brand/domain/model"
+	"ftd-td-catalog-item-read-services/internal/products-related/domain/model"
 	"github.com/gin-gonic/gin"
 )
 
-type SameBrand interface {
-	GetItemsBySameBrand(c *gin.Context, countryID, itemID string) ([]model.SameBrandItem, error)
+type ProductsRelated interface {
+	GetRelatedProducts(c *gin.Context, countryID, itemID string) ([]model.RelatedProduct, error)
 }
